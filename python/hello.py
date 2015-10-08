145fc9134d92bc4a069965d6f71cf59e9732c81c
Carman <11248039@qq.com>
2015-10-08 21:11:21 +0800

    add multi-threading function

--- python/hello.py
+++ python/hello.py
@@ -3,8 +3,6 @@ import os,getpass,threading,time,Queue
 from bs4 import BeautifulSoup
 
 HTTP_HEAD = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:38.0)Gecko/20100101 Firefox/38.0;Mozilla Firefox 38.2.0 - 10792--72'}
-proxy = {}
-H_PROXY = urllib2.ProxyHandler(proxy)
 H_HTTP = urllib2.HTTPHandler(0)
 
 #read proxy list from url and put it into a Queue
@@ -15,32 +13,36 @@ class Put_proxy(threading.Thread):
         self.q_addr = q_addr
 
     def run(self):
-        html = self.read_html(self.q_url.get())
+        while True:
+            url = self.q_url.get()
+            html = self.read_html(url)
+            self.get_proxy(html)
+            self.q_url.task_done()
+            
+    def get_proxy(self,html):
         soup=BeautifulSoup(html,'html') 
         mark=soup.find_all('tr',class_=True)
         for addr in mark:
             content=addr.contents[5].string+':'+\
                      addr.contents[7].string+' '+\
                      addr.contents[13].string 
-            self.q_addr.put(content)
-            print content,' put into queue.'
+            self.q_addr.put(content.lower())
+            #print content,' put into queue.'
 
     def read_html(self,url):
         req = urllib2.Request(url,None,HTTP_HEAD)
-        opener=urllib2.build_opener(H_PROXY)
+        opener=urllib2.build_opener()
         html=''
         try:
-            print 'reading web content from "%s" ...'%url
             response=opener.open(req)
             html=response.read()
-            print 'done.'
+            print 'Done reading from "%s" '%url 
         except urllib2.HTTPError as e:
             print 'Http: ',e
         except urllib2.URLError as e:
             print 'Url: ',e
             
         return html
- 
 
 
 #get proxy addr from the Queue and test it
@@ -50,65 +52,79 @@ class Test(threading.Thread):
         self.q_addr = q_addr
         
     def run(self):
-        addr = self.q_addr.get(0)
+        global cnt
+        while True:
+            self.test_proxy(self.q_addr.get())
+            self.q_addr.task_done()
+            time.sleep(0.1)
+            cnt=cnt+1
+
+    def test_proxy(self,addr):
         proxy={addr.split(' ')[1]:addr.split(' ')[0]}
         h_proxy=urllib2.ProxyHandler(proxy)
         opener=urllib2.build_opener(h_proxy,H_HTTP)
         try:
-            print '%s -->Testing proxy: %s ...'%(self.getName(),proxy)
-            #response=opener.open('http://www.google.com',timeout=1)
-            #print 'PASS.'
+            response=opener.open('http://www.google.com',timeout=5).read()
+            print '%s-->Testing: %s PASSED'%(self.getName(),proxy)
             lst.append(addr)
+            write_html('%s.html'%cnt,response)
         except Exception as e:
-            print 'FAIL.'
-            print e
-
+            print '%s-->Testing: %s FAILED'%(self.getName(),proxy)
+            #print e
     
-
+#write html
+def write_html(fname,html):
+    try:
+        f=open('/home/carman/Desktop/html/%s'%fname,'w')
+        f.write(html)
+        f.close
+        print 'Done writing html to %s'%fname
+    except Exception as e:
+        print 'Excp: ',e
+        
 #write proxy list to a text file
 def write_file(prolist,method='wb'):
     try:
         print 'writing proxy address to proxy.txt...'
-        f=open('proxy.txt',method)
-        cnt=1
+        f=open('/home/carman/Desktop/proxy.txt',method)
         for addr in prolist:
             f.write(addr+'\r\n')
-            cnt+=1
         f.close
         print 'done'
     except Exception as e:
         print 'Excp: ',e
 
 def main():
-    global lst
-    lst=[]
+    global lst,cnt
+    lst = []
+    cnt = 0
+    t0 = time.time()
+    
     que_addr = Queue.Queue()
     que_url = Queue.Queue()
 
     #put URLs to the que_url
-    sub=['nn']#,'nt','wn','wt']
+    sub=['nn','nt','wn','wt']
     for d in sub:
-        for i in range(1,3):
+        for i in range(1,2):
             url = 'http://www.xicidaili.com/%s/%s'%(d,str(i))
-            print url
             que_url.put(url)
 
-
-    while not que_url.empty():
-        th = Put_proxy(que_url,que_addr)
-        th.start()
-        time.sleep(0.3)
+    th = Put_proxy(que_url,que_addr)
+    th.start()
         
+    for x in xrange(10):
         th = Test(que_addr)
         th.start()
-        time.sleep(0.3)
+        time.sleep(0.1)
+    
+    que_url.join()
+    que_addr.join()
 
-        while True:
-            if (len(threading.enumerate()) < 5):
-                break
-            
+    print 'Tested proxy: %d'%cnt
+    print 'Elapsed time: %.2fs'%(time.time()-t0)
     write_file(lst)
-'''
-
+    
+   
 if __name__=='__main__':
     main()
--- python/xicidali.com.py
+++ python/xicidali.com.py
@@ -1,85 +1,130 @@
-#import sys
-#reload(sys)
-#sys.setdefaultencoding('utf-8')
 import urllib,urllib2
-import os
+import os,getpass,threading,time,Queue
 from bs4 import BeautifulSoup
 
-#test proxy
-def test_proxy(prolist):
-    tested_proxy=[]
-    h_http = urllib2.HTTPHandler(0)
-    for item in prolist:
-        proxy={item.split(' ')[1]:item.split(' ')[0]}
-        h_proxy=urllib2.ProxyHandler(proxy)
-        opener=urllib2.build_opener(h_proxy,h_http)
-        try:
-            print 'Testing proxy: %s ...'%proxy,
-            response=opener.open('http://www.google.com',timeout=1)
-            print 'PASS.'
-            tested_proxy.append(item)
-        except Exception as e:
-            print 'FAIL.'
-            print e
+HTTP_HEAD = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:38.0)Gecko/20100101 Firefox/38.0;Mozilla Firefox 38.2.0 - 10792--72'}
+H_HTTP = urllib2.HTTPHandler(0)
+
+#read proxy list from url and put it into a Queue
+class Put_proxy(threading.Thread):
+    def __init__(self,q_url,q_addr):
+        threading.Thread.__init__(self)
+        self.q_url = q_url
+        self.q_addr = q_addr
+
+    def run(self):
+        while True:
+            url = self.q_url.get()
+            html = self.read_html(url)
+            self.get_proxy(html)
+            self.q_url.task_done()
             
-    return tested_proxy
+    def get_proxy(self,html):
+        soup=BeautifulSoup(html,'html') 
+        mark=soup.find_all('tr',class_=True)
+        for addr in mark:
+            content=addr.contents[5].string+':'+\
+                     addr.contents[7].string+' '+\
+                     addr.contents[13].string 
+            self.q_addr.put(content.lower())
+            #print content,' put into queue.'
 
+    def read_html(self,url):
+        req = urllib2.Request(url,None,HTTP_HEAD)
+        opener=urllib2.build_opener()
+        html=''
+        try:
+            response=opener.open(req)
+            html=response.read()
+            print 'Done reading from "%s" '%url 
+        except urllib2.HTTPError as e:
+            print 'Http: ',e
+        except urllib2.URLError as e:
+            print 'Url: ',e
+            
+        return html
+
+
+#get proxy addr from the Queue and test it
+class Test(threading.Thread):
+    def __init__(self,q_addr):
+        threading.Thread.__init__(self)
+        self.q_addr = q_addr
+        
+    def run(self):
+        global cnt
+        while True:
+            self.test_proxy(self.q_addr.get())
+            self.q_addr.task_done()
+            time.sleep(0.1)
+            cnt=cnt+1
+
+    def test_proxy(self,addr):
+        proxy={addr.split(' ')[1]:addr.split(' ')[0]}
+        h_proxy=urllib2.ProxyHandler(proxy)
+        opener=urllib2.build_opener(h_proxy,H_HTTP)
+        try:
+            response=opener.open('http://www.google.com',timeout=5).read()
+            print '%s-->Testing: %s PASSED'%(self.getName(),proxy)
+            lst.append(addr)
+            #write_html('%s.html'%cnt,response)
+        except Exception as e:
+            print '%s-->Testing: %s FAILED'%(self.getName(),proxy)
+            #print e
     
-#read html
-def read_html(url):
-    user_agent='Mozilla/5.0 (Windows NT 6.1; WOW64; rv:38.0)Gecko/20100101 Firefox/38.0;Mozilla Firefox 38.2.0 - 10792--72'
-    header={'User-Agent':user_agent}
-    req = urllib2.Request(url,None,header)
-    html=''
+#write html
+def write_html(fname,html):
     try:
-        print 'reading web content from "%s" ...'%url
-        response=urllib2.urlopen(req)
-        html=response.read()
-        print 'done.'
-    except urllib2.HTTPError as e:
-        print 'Http: ',e
-    except urllib2.URLError as e:
-        print 'Url: ',e
-
-    return html
-
-def get_prolist(html):
-    prolist=[]
-    soup=BeautifulSoup(html,'lxml') 
-    mark=soup.find_all('tr',class_=True)
-    for addr in mark:
-        content=addr.contents[5].string+':'+\
-                 addr.contents[7].string+' '+\
-                 addr.contents[13].string 
-        prolist.append(content.lower())
-    #print str(prolist)
-    return prolist
-
+        f=open('/home/carman/Desktop/html/%s'%fname,'w')
+        f.write(html)
+        f.close
+        print 'Done writing html to %s'%fname
+    except Exception as e:
+        print 'Excp: ',e
+        
+#write proxy list to a text file
 def write_file(prolist,method='wb'):
     try:
         print 'writing proxy address to proxy.txt...'
         f=open('/home/carman/Desktop/proxy.txt',method)
-        cnt=1
         for addr in prolist:
             f.write(addr+'\r\n')
-            cnt+=1
         f.close
         print 'done'
     except Exception as e:
         print 'Excp: ',e
 
 def main():
+    global lst,cnt
+    lst = []
+    cnt = 0
+    t0 = time.time()
+    
+    que_addr = Queue.Queue()
+    que_url = Queue.Queue()
+
+    #put URLs to the que_url
     sub=['nn','nt','wn','wt']
     for d in sub:
-        for i in range(1,10):
-            url='http://www.xicidaili.com/%s/%s'%(d,str(i))
-            html=read_html(url)
-            prolist=get_prolist(html)
-            prolist=test_proxy(prolist)
-            if d=='nn' and i==1:
-                write_file(prolist) #clean old proxy list
-            else:
-                write_file(prolist,'ab')
+        for i in range(1,2):
+            url = 'http://www.xicidaili.com/%s/%s'%(d,str(i))
+            que_url.put(url)
 
+    th = Put_proxy(que_url,que_addr)
+    th.start()
+        
+    for x in xrange(10):
+        th = Test(que_addr)
+        th.start()
+        time.sleep(0.1)
+    
+    que_url.join()
+    que_addr.join()
+
+    print 'Tested proxy: %d'%cnt
+    print 'Elapsed time: %.2fs'%(time.time()-t0)
+    write_file(lst)
+    
+   
 if __name__=='__main__':
     main()
