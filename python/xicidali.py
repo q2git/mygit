6a18d3985fdeff36cc0397b56c2a798ece4e7c3b
q2git <54710@sohu.com>
2015-10-22 21:05:36 +0800

    Update xicidali.py

--- python/xicidali.py
+++ python/xicidali.py
@@ -1,75 +1,133 @@
-import urllib2
-import os,types,threading,time,Queue
+import urllib2,sqlite3
+import os,time,types
+import threading,Queue
 from bs4 import BeautifulSoup
 
-
+TESTRESULTS = []
+URL_TEST = 'http://www.google.com'
 HTTP_HEAD = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:38.0)Gecko/20100101 Firefox/38.0;Mozilla Firefox 38.2.0 - 10792--72'}
 H_HTTP = urllib2.HTTPHandler(0)
+LOCK = threading.Lock()
+################################################################################
+#get proxy addr from the Queue and test it
+class Test(threading.Thread):
+    def __init__(self,q_addr):
+        threading.Thread.__init__(self)
+        self.q_addr = q_addr
+        self.testresults = []
+        
+    def run(self):
+        global TESTRESULTS
+        while True:
+            proxy = self.q_addr.get()
+            if proxy is None:self.q_addr.put(None);break
+            self.test_proxy(proxy)
+        LOCK.acquire()
+        TESTRESULTS = TESTRESULTS + self.testresults
+        LOCK.release()
+        
+    def test_proxy(self,proxy):
+        host,port,ptype = proxy
+        proxy={ptype:host+':'+port}
+        h_proxy=urllib2.ProxyHandler(proxy)
+        opener=urllib2.build_opener(h_proxy,H_HTTP)
+        fails = 0
+        t0 = time.time()
+        while not(fails > 2):
+            try:
+                opener.open(URL_TEST,timeout=1).read(100)
+                break
+            except Exception as e:
+                time.sleep(1)
+                fails = fails + 1
+                #print e
+        speed = round(time.time() - t0,2)
+        pf = ['PASS','FAIL'][fails>2]
+        #(host,port,ptype,speed,pf)
+        self.testresults.append((host,port,ptype,speed,pf))
+        LOCK.acquire()
+        print '%s-%s->%s %sED in %.2fs'%(self.getName(),os.getpid(),proxy,pf,speed)
+        LOCK.release()
 
-#read proxy list from url and put into a list
+############################################################################
+#read proxy list from url and return a list
 def get_proxylist(url):
     proxylist = []
     try:
         print 'reading data from %s ...'%url,
         req = urllib2.Request(url,None,HTTP_HEAD)
         html = urllib2.urlopen(req,timeout=5).read()
-        print 'Done.'
-        
+        print 'Done.'      
         print 'analyzing web content ...',
         soup = BeautifulSoup(html,'html') 
         mark = soup.find_all('tr',class_=True)
         for item in mark:
-            pros = item.contents[5].string+':'+\
-                     item.contents[7].string+' '+\
-                     item.contents[13].string 
-            proxylist.append(pros.lower())
+            pros = (item.contents[5].string,
+                    item.contents[7].string,
+                    item.contents[13].string.lower())
+            #print pros
+            proxylist.append(pros)
         print 'Done.'
-        return proxylist
+        return proxylist #['host:port type',...]
         
     except urllib2.HTTPError as e:
         print 'Http: ',e
     except urllib2.URLError as e:
         print 'Url: ',e
-    except Exception, e:
-        print e
-
-
-#get proxy addr from the Queue and test it
-class Test(threading.Thread):
-    def __init__(self,q_addr):
-        threading.Thread.__init__(self)
-        self.q_addr = q_addr
+   # except Exception, e:
+       # print e
         
-    def run(self):
-        global cnt
-        while True:
-            self.test_proxy(self.q_addr.get())
-            self.q_addr.task_done()
-            cnt=cnt+1
-
-    def test_proxy(self,addr):
-        proxy={addr.split(' ')[1]:addr.split(' ')[0]}
-        h_proxy=urllib2.ProxyHandler(proxy)
-        opener=urllib2.build_opener(h_proxy,H_HTTP)
-        fails = 0
-        while not(fails > 2):
-            try:
-                t0 = time.time()
-                response=opener.open('http://www.google.com',timeout=5).read(100)
-                t1 = time.time()
-                lock.acquire()
-                print '%s-%s->%s PASSED @%.2fs'%(self.getName(),os.getpid(),proxy,(t1-t0))
-                lock.release()
-                lst.append(addr+' %.2f'%(t1-t0))
-                #write_html('%s.html'%(addr.split(' ')[0]),response.read())
-                break
-            except Exception as e:
-                fails = fails + 1
-                lock.acquire()
-                print '%s-%s->%s FAILED %d times'%(self.getName(),os.getpid(),proxy,fails)
-                lock.release()
-                #print e
+#########################################################################
+#########################################################################
+def open_conn(db = r'/home/carman/Desktop/proxy.db'):
+    table = ''
+    if not os.path.exists(db):
+        print 'Creating a new database in "%s" ...'%db
+        table = '''create table if not exists PROXY(
+            host varchar(50) primary key,
+            port varchar(10),
+            type varchar(10),
+            speed float,
+            pf varchar(10),
+            ftimes integer,
+            ptimes integer
+            )'''
+    conn = sqlite3.connect(db) 
+    conn.isolation_level = None #auto commit()
+    conn.execute(table)
+    return conn    
+############################
+#item = [(host,port,type,speed,pf),()...]    
+def db_update(items):   
+    if type(items)!=types.ListType:
+        print 'wrong type, List required.'
+        return
         
+    conn = open_conn()
+    cur = conn.cursor()
+    for item in items:
+        if type(item)!=types.TupleType: 
+            print 'wrong type, Tuple required.'
+            break
+        record = cur.execute("select host from PROXY where host=?",(item[0],)).fetchone()
+
+        if record is None:
+            print "Inserting new host %s ..."%item[0],
+            cur.execute('insert into PROXY values(?,?,?,?,?,0,0)',(item))
+            print 'Done.'
+        else:
+            print "Updating host %s ..."%item[0],
+            if item[4] == 'PASS':
+                cur.execute('update PROXY set port=?,type=?,speed=?,pf=?,ptimes=ptimes+1 where host=?',
+                            (item[1],item[2],item[3],item[4],item[0]))
+            elif item[4] == 'FAIL':
+                cur.execute('update PROXY set port=?,type=?,speed=?,pf=?,ftimes=ftimes+1 where host=?',
+                            (item[1],item[2],item[3],item[4],item[0]))
+            print 'Done.'
+                
+    cur.close()
+    conn.close()
+#################################################################################        
 #write html
 def write_html(fname,html):
     try:
@@ -91,39 +149,45 @@ def write_file(prolist,method='wb'):
         print 'done'
     except Exception as e:
         print 'Excp: ',e
-
+###############################################################################
 def main():
-    proxylist = []
-    cnt = 0
-    lock = threading.Lock()
-    t0 = time.time()
-    
-    que_addr = Queue.Queue()
+    t0 = time.time()    
+    que_proxy = Queue.Queue()
 
-    #put URLs to the que_url
+    #read urls and get proxylist
+    proxylist = []
     sub=['nn','nt','wn','wt']
     for d in sub:
-        for i in range(1,2):
+        for i in range(1,11):
             url = 'http://www.xicidaili.com/%s/%s'%(d,str(i))
             proxylist = proxylist + get_proxylist(url)
-    
+    #read proxylist from database and combine into one
 
-    
-    #
+    #put proxy into a queue
+    for item in proxylist:
+        que_proxy.put(item)
+    que_proxy.put(None) 
+    print 'started to testing the proxies...'
+    #start testing the proxy
+    ths = []
     for x in xrange(20):
-        th = Test(que_addr)
+        th = Test(que_proxy)
         th.start()
+        ths.append(th)
         #time.sleep(0.1)
-    
-    que_url.join()
-    que_addr.join()
+        
+    for th in ths:
+        th.join()
+
+    #que_proxy.join()
 
     print '-------------Test Report-------------'
-    print 'Tested proxy: %d'%cnt
-    print 'Passed proxy: %d'%len(lst)
+    print 'Tested proxy: %d'%len(TESTRESULTS)
+    print 'Passed proxy: %d'
     print 'Elapsed time: %.2fs'%(time.time()-t0)
-    write_file(lst)
-    
+    print TESTRESULTS
+    ##write_file(lst)
+    db_update(TESTRESULTS)
    
 if __name__=='__main__':  
     main()
