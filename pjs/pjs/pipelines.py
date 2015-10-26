5b120d6f2afa480bd2d63e4e75c1f622388d6486
Carman <11248039@qq.com>
2015-10-26 20:07:12 +0800

    modified pipelines.py

--- pjs/pjs/pipelines.py
+++ pjs/pjs/pipelines.py
@@ -4,8 +4,40 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import sqlite3
+from os import path
 
+from scrapy import signals
+from scrapy.xlib.pydispatch import dispatcher
 
-class PjsPipeline(object):
+class SQLiteStorePipeline(object):
+    filename = 'data.db'
+
+    def __init__(self):
+        self.conn = None
+        dispatcher.connect(self.initialize, signals.engine_started)
+        dispatcher.connect(self.finalize, signals.engine_stopped)
+
     def process_item(self, item, spider):
+        self.conn.execute('insert into TGB values(?,?,?)',
+                          (''.join(item['link']),''.join(item['title']),''.join(item['content'])))
         return item
+
+    def initialize(self):
+        if path.exists(self.filename):
+            self.conn = sqlite3.connect(self.filename)
+        else:
+            self.conn = self.create_table(self.filename)
+
+    def finalize(self):
+        if self.conn is not None:
+            self.conn.commit()
+            self.conn.close()
+            self.conn = None
+
+    def create_table(self, filename):
+        conn = sqlite3.connect(filename)
+        conn.execute("""create table TGB
+                     (url text, title text,content text)""")
+        conn.commit()
+        return conn
