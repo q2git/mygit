b46293afaded38feaed3a236beb419d6b4ed0d8e
q2git <54710@sohu.com>
2015-10-27 17:14:25 +0800

    Update items.py

--- pjs/pjs/items.py
+++ pjs/pjs/items.py
@@ -12,3 +12,8 @@ class PjsItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+class SapItem(scrapy.Item):
+    SAPPart = scrapy.Field()
+    Desc = scrapy.Field()
+    
