e65779d418a199d0d21f4a69ad94f33376c74340
q2git <54710@sohu.com>
2015-10-27 17:16:24 +0800

    Update pipelines.py
    example

--- pjs/pjs/pipelines.py
+++ pjs/pjs/pipelines.py
@@ -41,3 +41,56 @@ class SQLiteStorePipeline(object):
                      (url text, title text,content text)""")
         conn.commit()
         return conn
+        
+        
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+class BsPipeline(object):
+    def __init__(self):
+       self.file = open('temp.txt', 'wb')
+       
+    def process_item(self, item, spider):
+        self.file.write(item['SAPPart'].split()[0].strip()+'-->'+item['Desc'].strip()+ "\r\n")
+        return item
+        
+        
+class BsPipeline2(object):
+    def process_item(self, item, spider):
+        with open('temp1.txt', 'ab') as f:
+            f.writelines(str(item)+ "\r\n")
+        return item
+
+import json
+
+class JsonWriterPipeline(object):
+
+   def __init__(self):
+       self.file = open('items.txt', 'wb')
+
+   def process_item(self, item, spider):
+       line = json.dumps(dict(item)) + "\r\n"
+       self.file.write(line)
+       return item
+     
+from scrapy.exceptions import DropItem
+
+class DuplicatesPipeline(object):
+
+    def __init__(self):
+        self.ids_seen = set()
+
+    def process_item(self, item, spider):
+        #print type(item)
+        if item['title'][0]in self.ids_seen :
+            raise DropItem("Duplicate item found: %s" % item)
+        else:
+            self.ids_seen.add(item['title'][0])
+            return item
+ 
+     
