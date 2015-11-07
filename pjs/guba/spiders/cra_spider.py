c01d4eedc5b6bd5fd6e5ac3fb0016ba895ede45e
q2git <54710@sohu.com>
2015-11-07 15:11:48 +0800

    Update cra_spider.py

--- pjs/guba/spiders/cra_spider.py
+++ pjs/guba/spiders/cra_spider.py
@@ -9,28 +9,38 @@ class CrSpider(CrawlSpider):
     name = 'cr'
     allowed_domains = ['eastmoney.com']
     #start_urls = ['http://guba.eastmoney.com/news,600596,209423368.html']
-    start_urls = map(lambda x:'http://guba.eastmoney.com/list,600596_%d.html'%x,range(1,50))
+    start_urls = map(lambda x:'http://guba.eastmoney.com/list,002425_%d.html'%x,range(1,163))
     rules = (
         #Rule(LinkExtractor(allow=('default_\d+.html'))),
         #Rule(LinkExtractor(allow=('list,600596_\d+\.html',))),
-        Rule(LinkExtractor(allow=('news,600596,\d+\.html', )), callback='parse_item'),
+        Rule(LinkExtractor(allow=('news,\d{6},\d+\.html', )), callback='parse_item'),
     )
     #//div[ngbglistdiv]/ul[@ngblistul2]/li/@href 
     #<a href="topic,600000.html">(600000)浦发银行</a>
             
     def parse_item(self, response):
-        #self.logger.info('Hi, this is an item page! %s', response.url)
+        x = GubaItem()
+        x['Stock'] = response.url.split(',')[1]
         #return { 'LINK': response.url} |@id='zwlianame' |@id='zwlitime'
         #{'ID':response.xpath("//div[@id='zwconttbn']//text()").extract(),
         #       'Date':response.xpath("//div[@class='zwfbtime']/text()").extract()}
         for sel in response.xpath("//div[@class='zwli clearfix']|//div[@id='zwconttb']"):
-            ID = sel.xpath(".//span[@class='zwnick']//text()|\
+            x['ID'] = sel.xpath(".//span[@class='zwnick']//text()|\
             .//div[@id='zwconttbn']//text()").extract_first()
-            Date = sel.xpath(".//div[@class='zwlitime']/text()|\
+            x['Date'] = sel.xpath(".//div[@class='zwlitime']/text()|\
             .//div[@class='zwfbtime']/text()").re_first('\d{4}-\d{2}-\d{2}')
-            yield {'ID':ID,'Date':Date}
+            yield x #{'Stock':Stock,'ID':ID,'Date':Date}
  
 if __name__ == '__main__':
+        #for fixing import error    
+    if __package__ is None:
+        import sys
+        from os import path
+        sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
+        from items import GubaItem
+    else:
+        from guba.items import GubaItem
+        
     process = CrawlerProcess(get_project_settings())   
     process.crawl(CrSpider)
     process.start() # the script will block here until the crawling is finished
